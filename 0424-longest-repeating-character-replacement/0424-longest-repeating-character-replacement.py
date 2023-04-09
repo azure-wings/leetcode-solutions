399d35072690e2a9896fee979591bc1743c67c89
azurewings <70371393+azure-wings@users.noreply.github.com>
2023-04-09 16:00:57 +0900

    Time: 108 ms (88.18%), Space: 13.9 MB (89.9%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,31 +1,23 @@
-from dataclasses import dataclass
-from typing import Dict
-
-
-@dataclass
-class LetterCount:
-    left: int = 0
-    max_len: int = 0
-    other_char: int = 0
+from collections import defaultdict
+from typing import DefaultDict
 
 
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        letters: Dict[str, LetterCount] = {c: LetterCount() for c in s}
+        left: int = 0
+        counts: DefaultDict[str, int] = defaultdict(int)
+        max_len: int = 0
 
         for right in range(len(s)):
-            for c in letters:
-                if c != s[right]:
-                    letters[c].other_char += 1
-                    while letters[c].other_char > k:
-                        if s[letters[c].left] != c:
-                            letters[c].other_char -= 1
-                        letters[c].left += 1
-
-                letters[c].max_len = (
-                    right - letters[c].left + 1
-                    if right - letters[c].left + 1 > letters[c].max_len
-                    else letters[c].max_len
+            counts[s[right]] += 1
+            curr_len: int = right - left + 1
+            if curr_len - max(counts.values()) <= k:
+                max_len = (
+                    curr_len if curr_len > max_len
+                    else max_len
                 )
+            else:
+                counts[s[left]] -= 1
+                left += 1
 
-        return max(letters[c].max_len for c in letters)
+        return max_len

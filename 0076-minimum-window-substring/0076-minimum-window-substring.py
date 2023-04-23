eb5480c1650c6af03a6bd199d40e157008e9af5d
azurewings <70371393+azure-wings@users.noreply.github.com>
2023-04-23 18:00:46 +0900

    Time: 200 ms (29.35%), Space: 14.7 MB (71.90%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -9,18 +9,23 @@ class Solution:
         min_str: str = ""
         counter_given: Counter = Counter(t)
         counter_curr: Counter = Counter()
+        matched: int = 0
 
         for right in range(m):
             if s[right] in counter_given:
                 counter_curr[s[right]] += 1
+                if counter_curr[s[right]] == counter_given[s[right]]:
+                    matched += 1
 
-            while counter_curr & counter_given == counter_given:
+            while matched == len(counter_given):
                 if min_str_len > right - left + 1:
                     min_str = s[left : right + 1]
                     min_str_len = right - left + 1
     
                 if s[left] in counter_given:
                     counter_curr[s[left]] -= 1
+                    if counter_curr[s[left]] == counter_given[s[left]] - 1:
+                        matched -= 1
                 left += 1
 
         return min_str

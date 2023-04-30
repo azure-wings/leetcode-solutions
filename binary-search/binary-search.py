6553152f7d40051ad24eb7b9e0ff002838f3327c
azurewings <70371393+azure-wings@users.noreply.github.com>
2023-04-30 17:03:43 +0900

    Time: 235 ms (87.60%) | Memory: 18 MB (14.04%) - LeetSync

--- binary-search/binary-search.py
+++ binary-search/binary-search.py
@@ -3,21 +3,13 @@ class Solution:
         lo, hi = 0, len(nums) - 1
 
         while lo <= hi:
-            mid = lo + ((hi - lo) // 2)
-
-            if mid == lo:
-                if nums[lo] == target:
-                    return lo
-                elif nums[hi] == target:
-                    return hi
-                else:
-                    break
+            mid = (lo + hi) // 2
             
             if target == nums[mid]:
                 return mid
             elif target < nums[mid]:
-                hi = mid
+                hi = mid - 1
             else:
-                lo = mid
+                lo = mid + 1
         
         return -1

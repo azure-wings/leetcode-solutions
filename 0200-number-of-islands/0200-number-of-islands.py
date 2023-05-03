ed2c11ac5be13b823b48c9dd6b9b038dd28d712e
azurewings <70371393+azure-wings@users.noreply.github.com>
2023-05-03 20:35:52 +0900

    Time: 301 ms (63.32%), Space: 18.9 MB (39.76%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,39 +1,24 @@
-from collections import deque
-from typing import Deque, Tuple, Set
-
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        num_rows, num_cols = len(grid), len(grid[0])
         count: int = 0
 
-        in_range: Callable[[int, int], int] = lambda idx: 0 <= idx[0] < len(
-            grid
-        ) and 0 <= idx[1] < len(grid[0])
+        def dfs(row: int, col: int) -> None:
+            if grid[row][col] == "1":
+                grid[row][col] = "0"
+                if row > 0:
+                    dfs(row - 1, col)
+                if row < num_rows - 1:
+                    dfs(row + 1, col)
+                if col < num_cols - 1:
+                    dfs(row, col + 1)
+                if col > 0:
+                    dfs(row, col - 1)
 
-        dx: List[int] = [-1, 1, 0, 0]
-        dy: List[int] = [0, 0, -1, 1]
-
-        visiting: Deque[Tuple[int, int]] = deque()
-        visited: Set[Tuple[int, int]] = set()
-
-        def dfs() -> None:
-            while visiting:
-                curr_row, curr_col = visiting.pop()
-                visited.add((curr_row, curr_col))
-                for i in range(4):
-                    next_row, next_col = curr_row + dx[i], curr_col + dy[i]
-                    if (
-                        in_range((next_row, next_col))
-                        and grid[next_row][next_col] == "1"
-                        and (next_row, next_col) not in visited
-                    ):
-                        visiting.append((next_row, next_col))
-
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                if grid[row][col] == "1" and (row, col) not in visited:
-                    visiting.append((row, col))
-                    dfs()
+        for row in range(num_rows):
+            for col in range(num_cols):
+                if grid[row][col] == "1":
                     count += 1
+                    dfs(row, col)
 
         return count

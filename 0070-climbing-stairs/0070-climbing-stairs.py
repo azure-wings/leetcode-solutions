8720838a961bea3d8e0de35d865adc7d1e78331f
azurewings <70371393+azure-wings@users.noreply.github.com>
2023-05-03 20:42:24 +0900

    Time: 48 ms (5.33%), Space: 16.2 MB (7.76%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+
+        memo: List[int] = [None] * n
+        memo[0] = 1
+        memo[1] = 2
+
+        for i in range(2, n):
+            memo[i] = memo[i - 1] + memo[i - 2]
+
+        return memo[n - 1]
